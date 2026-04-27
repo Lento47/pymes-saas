5b6dd16f10bf42dedba48ec017d5c6ddb75d3e71
lento47 <lejzerv@gmail.com>
2026-04-27 09:52:49 -0600

    fix: use forwardRef for AuthModule in SamlModule, remove duplicate ConfigModule

--- apps/api/src/auth/saml/saml.module.ts
+++ apps/api/src/auth/saml/saml.module.ts
@@ -1,12 +1,11 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { Module, forwardRef } from '@nestjs/common';
 import { SamlController } from './saml.controller';
 import { SamlService } from './saml.service';
 import { PrismaModule } from '../../common/prisma/prisma.module';
 import { AuthModule } from '../auth.module';
 
 @Module({
-  imports: [ConfigModule, PrismaModule, AuthModule],
+  imports: [PrismaModule, forwardRef(() => AuthModule)],
   controllers: [SamlController],
   providers: [SamlService],
 })
