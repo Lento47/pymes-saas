fcac5c5bdbadf3805c622fa222a39a58aabb3548
Lejzer <89803550+Lento47@users.noreply.github.com>
2026-05-30 20:47:27 -0600

    fix: load mobile sidebar logout styles

--- apps/web/client/src/main.tsx
+++ apps/web/client/src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "@/components/layout/sidebar-mobile-logout.css";
 import { installGlobalErrorReporting } from "@/lib/error-reporting";
 import { normalizeInitialLocation } from "@/hooks/use-workspace-location";
 
